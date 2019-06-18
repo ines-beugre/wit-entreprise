66c85a0f352ea362c00f3c9cf473709472b3a42d
Inès <ines.beugre@serli.com>
2019-06-18 23:57:10 +0200

    debut de formation details

--- src/main/resources/app/components/formation/formation.jsx
+++ src/main/resources/app/components/formation/formation.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {getFormation, getFormations} from "../../redux/formations/dispatch";
 import IsPending from "../isPending/isPending";
+//import moment from 'moment/locale/fr';
+import moment from 'moment';
 import './formation.css'
 
 class Formation extends React.Component {
@@ -12,16 +14,22 @@ class Formation extends React.Component {
 
     render() {
         const {formation, isPending} = this.props;
+
         return(
           <div className="formation-container">
-              formation unique
+              {/*<h3>Détails</h3>*/}
               {
                   isPending ?
                   <IsPending className="formations-pending"/>
                   :
                       <div className="formation">
-                          <div className="formation-image">
-                              <img src = {formation.image}/>
+                          <div >
+                              <div className="formation-theme-date">
+                                  <div className="formation-theme">{formation.theme}</div>
+                                  <div className="formation-date">{moment(formation.date).locale('fr').format('DD MMMM YYYY')} </div>
+                              </div>
+
+                              <img className="formation-image" src = {formation.image}/>
                           </div>
 
 
