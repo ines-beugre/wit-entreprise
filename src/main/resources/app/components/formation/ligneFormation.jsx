630477da7131e14605eed494f9d169800ef35b45
Beugré <ines.beugre@serli.com>
2019-08-13 15:37:32 +0200

    --

--- src/main/resources/app/components/formation/ligneFormation.jsx
+++ src/main/resources/app/components/formation/ligneFormation.jsx
@@ -6,21 +6,19 @@ import 'moment/locale/fr';
 const LigneFormation = ({formation}) => {
 
     return (
-        <div key={formation.id} className="ligne-formation">
-            <Link to={`/formations/${formation.id}`}>
-                {/*<div className="ligne-formation-unique">*/}
-                    <div className="formations-image">
-                        <img className="image" src={formation.image}/>
-                    </div>
+        <Link to={`/formations/${formation.id}`}>
+            <div key={formation.id} className="ligne-formation">
+                <div className="formations-image">
+                    <img className="image" src={formation.image}/>
+                </div>
 
-                    <div className="formations-content">
-                        <div className="formations-name">{formation.name}</div>
-                        <div className="formations-date">{formation.date}</div>
-                    </div>
-                {/*</div>*/}
+                <div className="formations-content">
+                    <div className="formations-name">{formation.name}</div>
+                    <div className="formations-date">{formation.date}</div>
+                </div>
 
-            </Link>
-        </div>
+            </div>
+        </Link>
     )
 };
 
